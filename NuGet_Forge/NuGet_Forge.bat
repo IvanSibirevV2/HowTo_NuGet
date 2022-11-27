43c01a70ce53a04cd435d2694d5ead4bbaa9292d
qwe <qwe>
2022-11-27 14:21:16 +0300

    <id>NuGet_Forge</id><version>0.0.12</version>

--- NuGet_Forge/NuGet_Forge.bat
+++ NuGet_Forge/NuGet_Forge.bat
@@ -1,87 +1,54 @@
-Rem V0.0.2
+::Рассматриваем возможность того что 
+::Менюшка может быть сложнее чем просто папка с микробатниками
 @echo off
 cd %~dp0
-if "%cd%" EQU "C:\Program Files\Notepad++" (
-	cd C:\D\Git_Hub\HowTo_NuGet\NuGet_Forge
-)
 echo %cd%
-call :menu.run
-
+call :menu_03268
 PAUSE
-CLS
-EXIT
+exit /b
 
-rem s - doun
-:menu.Choice2
-	set /a menu.id= %menu.id%-1
-exit /b 0
-rem e - DO
-:menu.Choice3
-	set /a menu.id= %menu.id%+1
-exit /b 0
-rem Show Menu
-:menu.run
-	set menu.id=1
-	:menu.run.m_cajhgushfkjdagskjsagj
-		echo ##################################
-		call :menu.Choice1
-		echo ##################################
-		choice /c ews
-		call :menu.Choice%ErrorLevel% do
-	goto :menu.run.m_cajhgushfkjdagskjsagj
-exit /b 0
-rem w - upp
-rem Пункты меню вписывать сюда.
-:menu.Choice1
-	set menu.counter=-1
-	:::::::::::::::::::::::::::::::::::::::::::::::::::
-	set /a menu.counter= %menu.counter%+1
-	if "%1" EQU "" (
-		if %menu.id% EQU %menu.counter% (echo * 0. Exit)
-		if %menu.id% NEQ %menu.counter% (echo 0. Exit)
-	)else (if %menu.id% EQU %menu.counter% Exit)
-	:::::::::::::::::::::::::::::::::::::::::::::::::::
-	set /a menu.counter= %menu.counter%+1
-	if "%1" EQU "" (
-		if %menu.id% EQU %menu.counter% (echo * nuget spec)
-		if %menu.id% NEQ %menu.counter% (echo nuget spec)
-	)else (if %menu.id% EQU %menu.counter% nuget spec)
-	:::::::::::::::::::::::::::::::::::::::::::::::::::
-	set /a menu.counter= %menu.counter%+1
-	if "%1" EQU "" (
-		if %menu.id% EQU %menu.counter% (echo * notepad Package.nuspec)
-		if %menu.id% NEQ %menu.counter% (echo notepad Package.nuspec)
-	)else (if %menu.id% EQU %menu.counter% (notepad Package.nuspec)&&(nuget pack))
-	:::::::::::::::::::::::::::::::::::::::::::::::::::
-	set /a menu.counter= %menu.counter%+1
-	if "%1" EQU "" (
-		if %menu.id% EQU %menu.counter% (echo * notepad NuGet_Forge_ReadMe.txt)
-		if %menu.id% NEQ %menu.counter% (echo notepad NuGet_Forge_ReadMe.txt)
-	)else (if %menu.id% EQU %menu.counter% notepad NuGet_Forge_ReadMe.txt)
-	:::::::::::::::::::::::::::::::::::::::::::::::::::
-	set /a menu.counter= %menu.counter%+1
-	if "%1" EQU "" (
-		if %menu.id% EQU %menu.counter% (echo * nuget pack)
-		if %menu.id% NEQ %menu.counter% (echo nuget pack)
-	)else (if %menu.id% EQU %menu.counter% nuget pack)
-	:::::::::::::::::::::::::::::::::::::::::::::::::::
-	set /a menu.counter= %menu.counter%+1
-	if "%1" EQU "" (
-		if %menu.id% EQU %menu.counter% (echo * nuget delete /?)
-		if %menu.id% NEQ %menu.counter% (echo nuget delete /?)
-	)else (if %menu.id% EQU %menu.counter% nuget delete /?)
-	:::::::::::::::::::::::::::::::::::::::::::::::::::
-	set /a menu.counter= %menu.counter%+1
-	if "%1" NEQ "" (
-		if %menu.id% EQU %menu.counter% (
-			Set /p packageID= .packageID=
-			Set /p packageVersion= .packageVersion=
-		)
-	)
-	if "%1" EQU "" (
-		if %menu.id% EQU %menu.counter% echo * nuget delete ^<packageID^> ^<packageVersion^> -apikey [...] -Source [...]
-		if %menu.id% NEQ %menu.counter% echo nuget delete ^<packageID^> ^<packageVersion^> -apikey [...] -Source [...]
-	)else (if %menu.id% EQU %menu.counter% nuget delete %PackageID% %packageVersion% -Source https://www.nuget.org/ -apikey %menu.APIKey%)
-	::)else (if %menu.id% EQU 5 nuget delete -apikey oy2a42l6o4oyrf2onb5agpcnybjdndmevkqootw2luis4q)
-	::-apikey
-exit /b 0
+:menu_03268
+setlocal
+	set _id=0
+	:menu_03268_begin
+	set _counter=-1
+	cls
+	:::::::::::::::::::::::::::::::::::::::::::::::
+		set /a _counter= %_counter%+1
+		:::::::::::::::::::::::::::::::::::::::::::::::::::
+		if "%1" EQU "" ((if %_id% EQU %_counter% (<nul set /p strTemp=*))&(echo Exit)
+		)else (if %_id% EQU %_counter% (echo Exit)&(TIMEOUT /T 2)&(Exit))
+		:::::::::::::::::::::::::::::::::::::::::::::::
+		set /a _counter= %_counter%+1
+		if "%1" EQU "" ((if %_id% EQU %_counter% (<nul set /p strTemp=*))&(echo nuget spec)
+		)else (if %_id% EQU %_counter% (nuget spec)&(notepad Package.nuspec))
+		:::::::::::::::::::::::::::::::::::::::::::::::
+		set /a _counter= %_counter%+1
+		if "%1" EQU "" ((if %_id% EQU %_counter% (<nul set /p strTemp=*))&(echo notepad Package.nuspec)
+		)else (if %_id% EQU %_counter% (notepad Package.nuspec))
+		:::::::::::::::::::::::::::::::::::::::::::::::
+		set /a _counter= %_counter%+1
+		if "%1" EQU "" ((if %_id% EQU %_counter% (<nul set /p strTemp=*))&(echo notepad NuGet_Forge_ReadMe.txt)
+		)else (if %_id% EQU %_counter% (notepad NuGet_Forge_ReadMe.txt))
+		:::::::::::::::::::::::::::::::::::::::::::::::
+		set /a _counter= %_counter%+1
+		if "%1" EQU "" ((if %_id% EQU %_counter% (<nul set /p strTemp=*))&(echo nuget pack)
+		)else (if %_id% EQU %_counter% (nuget pack)&(TIMEOUT /T 10))
+		:::::::::::::::::::::::::::::::::::::::::::::::
+		set /a _counter= %_counter%+1
+		if "%1" EQU "" ((if %_id% EQU %_counter% (<nul set /p strTemp=*))&(echo nuget delete /?)
+		)else (if %_id% EQU %_counter% (echo nuget delete not provided at oll)&(TIMEOUT /T 10))
+		:::::::::::::::::::::::::::::::::::::::::::::::
+		set /a _counter= %_counter%+1
+		if "%1" EQU "" ((if %_id% EQU %_counter% (<nul set /p strTemp=*))&(echo git clone ... for: Sources, Examples, Help)
+		)else (if %_id% EQU %_counter% (git clone https://github.com/IvanSibirevV2/HowTo_NuGet.git)&(TIMEOUT /T 10))
+	:::::::::::::::::::::::::::::::::::::::::::::::
+	if "%1" EQU "Do" (exit /b) else (choice /c ews)
+	if %ErrorLevel% EQU 1 ((echo e)&(call :menu_03268_begin Do)&(set %ErrorLevel%=0))
+	if %ErrorLevel% EQU 2 ((echo w)&(set /a _id= %_id%-1)&(set %ErrorLevel%=0))
+	if %ErrorLevel% EQU 3 ((echo s)&(set /a _id= %_id%+1)&(set %ErrorLevel%=0))
+	if %_id% LSS 0 set _id=%_counter%
+	if %_id% GTR %_counter% set _id=0
+	goto :menu_03268_begin
+endlocal
+exit /b
